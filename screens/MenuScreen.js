73044b12885b707b25b6145e313dca622c72f0ac
Oscar Ossa <oskrlex17@gmail.com>
2021-11-22 19:17:24 -0500

    working on swiper menu

--- screens/MenuScreen.js
+++ screens/MenuScreen.js
@@ -1,45 +1,14 @@
 import React from 'react';
 import {useNavigation} from '@react-navigation/core';
-import {View, Text, TouchableOpacity, Button} from 'react-native';
+import {View, Text, TouchableOpacity, Button, StyleSheet} from 'react-native';
 import tw from 'twrnc';
 import Asistencia from '../components/menu/Asistencia';
-import Swiper from 'react-native-deck-swiper';
-
+import Swiper from 'react-native-swiper';
 const MenuScreen = () => {
   const navigation = useNavigation();
   return (
     <View style={tw.style('flex-1')}>
-      <View style={tw.style('flex-row justify-around mt-10')}>
-        {/* Menu */}
-        <View>
-          <TouchableOpacity onPress={() => navigation.navigate('Inicio')}>
-            <Text style={tw.style('text-lg font-semibold')}>Inicio</Text>
-            {/*Componente Inicio*/}
-          </TouchableOpacity>
-        </View>
-        <View>
-          <TouchableOpacity>
-            <Text style={tw.style('text-lg font-semibold')}>Asistencia</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View>
-          <TouchableOpacity onPress={() => navigation.navigate('Contacto')}>
-            <Text style={tw.style('text-lg font-semibold')}>Contacto</Text>
-            {/*Componente Contacto*/}
-          </TouchableOpacity>
-        </View>
-
-        <View>
-          <TouchableOpacity onPress={() => navigation.navigate('Esta App')}>
-            <Text style={tw.style('text-lg font-semibold')}>Esta app</Text>
-            {/*Componente Esta app*/}
-          </TouchableOpacity>
-        </View>
-        {/* End of Menu */}
-      </View>
-
-      <View style={tw.style('flex-1 -mt-6')}>
+      {/* <View style={tw.style('flex-1 -mt-6')}>
         <Swiper
           containerStyle={{backgroundColor: "transparent"}}
           cards={['DO', 'MORE', 'OF', 'WHAT', 'MAKES', 'YOU', 'HAPPY']}
@@ -54,9 +23,49 @@ const MenuScreen = () => {
           backgroundColor={'#FFFF'}
           stackSize={3}>
         </Swiper>
+      </View> */}
+      <View style={tw.style('flex-1')}>
+        <Swiper style={styles.wrapper} showsButtons={true}>
+          <View style={styles.slide1}>
+            <Text style={styles.text}>Hello Swiper</Text>
+          </View>
+          <View style={styles.slide2}>
+            <Text style={styles.text}>Beautiful</Text>
+          </View>
+          <View style={styles.slide3}>
+            <Text style={styles.text}>And simple</Text>
+          </View>
+        </Swiper>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {},
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
 
 export default MenuScreen;
